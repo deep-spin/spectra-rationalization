cc21ee29e9e2c53be6d1f7d1f2376f3ddc26c961
Marcos Treviso <marcosvtreviso@gmail.com>
2022-12-06 08:21:23 +0000

    Add datasets

--- rationalizers/data_modules/__init__.py
+++ rationalizers/data_modules/__init__.py
@@ -6,7 +6,9 @@ from rationalizers.data_modules.contrast_imdb_cf import CounterfactualContrastIM
 from rationalizers.data_modules.hans import HANSDataModule
 from rationalizers.data_modules.hotel_location import HotelLocationDataModule
 from rationalizers.data_modules.imdb import ImdbDataModule
-from rationalizers.data_modules.imdb_augmented import AugmentedImdbDataModule
+from rationalizers.data_modules.imdb_aug import AugmentedImdbDataModule
+from rationalizers.data_modules.imdb_syn import SyntheticImdbDataModule
+from rationalizers.data_modules.imdb_syn_exp import SyntheticExplainImdbDataModule
 from rationalizers.data_modules.mlqepe import MLQEPEDataModule
 from rationalizers.data_modules.multinli import MultiNLIDataModule
 from rationalizers.data_modules.revised_imdb import RevisedIMDBDataModule
@@ -35,6 +37,8 @@ available_data_modules = {
     "ag_news": AgNewsDataModule,
     "imdb": ImdbDataModule,
     "imdb_augmented": AugmentedImdbDataModule,
+    "imdb_synthetic": SyntheticImdbDataModule,
+    "imdb_synthetic_exp": SyntheticExplainImdbDataModule,
     "multi_nli": MultiNLIDataModule,
     "hotel_location": HotelLocationDataModule,
     "hans": HANSDataModule,
